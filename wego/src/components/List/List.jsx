1c45ec4806d12d03ea2a2e646f47e4f9ad8a07f3
yeeahG <yeji.sep.k@gmail.com>
2022-04-17 02:52:07 +0900

    [List]Make Type, Rating

--- wego/src/components/List/List.jsx
+++ wego/src/components/List/List.jsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { CircularProgress, Grid, Typography, MenuItem, FormControl, Select, InputLabel } from '@material-ui/core'
 
 const List = () => {
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+
   return (
     <div>
-        <h3>List</h3>
+        <Typography variant="h4">
+          Restaurants, Hotels & Attractions around you
+        </Typography>
 
+        <FormControl>
+          <InputLabel>Type</InputLabel>
+          <Select value={type} onChange={(e) => setType(e.target.value)}>
+            <MenuItem value="restaurants">Restaurants</MenuItem>
+            <MenuItem value="hotels">Hotels</MenuItem>
+            <MenuItem value="attractions">Attractions</MenuItem>
+          </Select>
+        </FormControl>
+
+        <FormControl>
+          <InputLabel>Rating</InputLabel>
+          <Select value={rating} onChange={(e) => setRating(e.target.value)}>
+            <MenuItem value={0}>All</MenuItem>
+            <MenuItem value={3}>Above 3.0</MenuItem>
+            <MenuItem value={4}>Above 4.0</MenuItem>
+            <MenuItem value={4.5}>Above 4.5</MenuItem>
+          </Select>
+        </FormControl>
     </div>
   )
 }
